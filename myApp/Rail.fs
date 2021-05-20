314f7aa0ccd3b46ba788a8e11b7a1c2dd49878b7
calebhhh <caleb.h@outlook.com>
2021-05-20 08:25:24 -0500

    updated railTests

--- myApp/Rail.fs
+++ myApp/Rail.fs
@@ -12,7 +12,13 @@ open System
     let createStation name lat lon =
         {name = name; id = Name(name) ; lat = lat; lon = lon}
 
-    type Line = {name : string; stops : List<Station>}
+    type Line = 
+        {name : string; stops : List<Station>}
+
+        member self.stationIndex (s: Station) : option<int> = 
+            match self.stops |> List.contains s with
+            | true -> self.stops |> List.tryFindIndex (fun x -> x = s)
+            | _ -> option<int>.None
 
     // takes the first and last stop of a line; along with a name -> outputs a Line
     let createLine name first last = 
@@ -20,24 +26,13 @@ open System
         {name = name; stops = stopsToLine}
 
     // here, 'after' refers the previous station to which you are adding the new station
-    let addStop (line: Line) (after: Station) (newStop: Station) = 
-        failwith "not implemented yet"
-        let i =
-            match line.stops |> List.contains after with
-                | true -> line.stops |> List.findIndex (fun x -> x = after)
-                | _ -> -1
-        if i > -1 then
-            let n =
-                line.stops
-                |> List.splitAt i
-            let newLine = 
-                {name = line.name ; stops = List.concat [fst n; [newStop] ; snd n]} // needs to update in database ; nyi
-            let responseT = "successfuly created line"
-            printfn $"{responseT}"
-        else
-            failwith "given station does not exist; new station cannot be
-                      added at a station that is not in the given line"
-
+    // You may simply pass the index of line[i] as 'after' ; or add after an explicit Station
+    let addStop (line: Line) (after: Station) (newStop: Station) =
+        match line.stationIndex after with
+        | Some i -> let n = line.stops |> List.splitAt (i+1) 
+                    option<Line>.Some {name = line.name ; stops = List.concat [fst n; [newStop] ; snd n]}
+        | _ -> option<Line>.None
+        
     // attempts to store a created line, returns a string that indicates whether 
     // that line exists already or was successfully created
     let storeLine (line: Line) =
--- tests/RailTests.fs
+++ tests/RailTests.fs
@@ -4,6 +4,55 @@ open myApp.Rail
 open System
 open Xunit
 
+let station1 = {
+        name = "São Bento";
+        id = Name("São Bento");
+        lat = 5.0;
+        lon = 5.0
+    }
+
+let station2 = {
+    name = "Orientale";
+    id = Id(7);
+    lat = 4.0;
+    lon = 1.0;
+}
+
+let station3 = {
+    name = "Lisboa Campanhã";
+    id = Name("Lisboa Campanhã");
+    lat = 3.5;
+    lon = 1.1
+}
+
+let station4 = {
+    name = "Matosinhos";
+    id = None;
+    lat = 9.2;
+    lon = 0.4
+}
+
+let line1 = {
+    name = "primeira linha";
+    stops = [
+        station1;
+        station2;
+        station3;
+        station4
+    ]
+}
+
+let station5 = createStation "Colisseu Centro" 33.2 2.3
+
+let station6 = {
+        name = "Matosinhos";
+        id = None;
+        lat = 9.2;
+        lon = 0.4
+    }
+
+let station7 = createStation "Matosinhos" 9.2 0.4
+
 [<Fact>]
 let ``createStationTest1`` () =
     let name, id, lat, lon = "São Bento", 12, 5.0, 5.0
@@ -27,3 +76,35 @@ let ``createStationTest2`` () =
     }
     let actualStation = createStation name lat lon
     Assert.True(actualStation.Equals(expectedStation))
+
+[<Fact>]
+let ``createLineTest1`` () = 
+    let expectedStation = createLine "line 1" station1 station2
+    let actualStation = {
+        name = "line 1";
+        stops = [station1; station2]
+    }
+    Assert.True(actualStation.Equals(expectedStation))
+
+[<Fact>]
+let ``createLineTest2`` () =
+    let expectedStation = createLine "line 1" station1 station3
+    let actualStation = {
+        name = "line 1";
+        stops = [station1; station2]
+    }
+    Assert.False(actualStation.Equals(expectedStation))
+
+[<Fact>]
+let ``addStopTest1`` () =
+    let expected = line1
+    let start = createLine "primeira linha" station1 station4
+    let step2 = addStop start station1 station3
+    let step3 = step2.Value
+    let step4 = addStop step3 station1 station2
+    let actual = step4.Value
+    Assert.Equal(expected, actual)
+
+// [<Fact>]
+// let ``addStopTest2`` () =
+//     failwith "not implemented yet"
